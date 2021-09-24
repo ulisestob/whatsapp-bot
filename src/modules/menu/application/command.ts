f788ed1b48c84d9111fcd117e780f430ce64c0c9
Ulises Tobar <ulises.se.tobar@gmail.com>
2021-09-23 22:51:54 -0600

    feat(project): support sticker from video, demos

--- src/modules/menu/application/command.ts
+++ src/modules/menu/application/command.ts
@@ -8,9 +8,11 @@ class MenuCommand implements Command<string> {
         const commands = [
             '*!help*: _muestra el menu de commandos._',
             '*!sticker*: _convierte cualquier imagen, gif, video en sticker._',
-            '*!survey*: _genera una encuesta rapida._',
+            '*!heroes*: _muestra una demo de formulario._',
+            '*!plan*: _demo de botones._',
         ]
-        return `⌘⌘⌘⌘ *MENU* ⌘⌘⌘⌘\n\n${commands.join(`\n`)}\n\n⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘`
+        const footer = '\n\n🕑 *Horario:* _7:00AM - 10:00PM_\n\n'
+        return `⌘⌘⌘⌘⌘ *MENU* ⌘⌘⌘⌘⌘\n\n${commands.join(`\n`)}\n\n${footer}⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘`
     }
 
 }
--- src/modules/shared/infrastructure/baileys/baileys.ts
+++ src/modules/shared/infrastructure/baileys/baileys.ts
@@ -1,6 +1,12 @@
 import { WAConnection, MessageType, WAChatUpdate } from '@adiwajshing/baileys'
 import Command from '../../domain/command'
 import sharp from 'sharp'
+import streamifier from 'streamifier'
+import ffmpegInstaller from '@ffmpeg-installer/ffmpeg'
+import fluent from 'fluent-ffmpeg'
+import tmp from 'tmp'
+import fs from 'fs'
+const ffprobe = require('@ffprobe-installer/ffprobe')
 
 class Baileys {
     private connection: WAConnection
@@ -46,7 +52,7 @@ class Baileys {
     }
 
     public attachSticker () {
-        console.log('attached: !sticker')
+        console.log('attached: !sticker : Image')
         this.connection.on('chat-update', async (chat: WAChatUpdate) => {
             if (chat.messages && chat.count) {
                 const message = chat.messages.last
@@ -63,6 +69,121 @@ class Baileys {
                         }).webp({ lossless: true }).toBuffer()
 
                     await this.send(id, <any>sticker, MessageType.sticker)
+                }
+            }
+        })
+        return this
+    }
+
+    public attachStickerVideo () {
+        console.log('attached: !sticker : Video')
+        this.connection.on('chat-update', async (chat: WAChatUpdate) => {
+            if (chat.messages && chat.count) {
+                const message = chat.messages.last
+                const [ messageType ] = Object.keys(<any>message.message)
+                const id: string = <string>message.key.remoteJid
+                const { url, caption } = message.message?.videoMessage || {}
+
+                if (messageType == MessageType.video && url && caption == '!sticker') {
+                    const videoBuffer = await this.connection.downloadMediaMessage(message)
+                    const videoStream = await streamifier.createReadStream(videoBuffer)
+                    
+                    const tempObject = tmp.fileSync()
+                    const tempName = `${tempObject.name}.webp`
+
+                    const ffmpeg = fluent()
+                        .setFfprobePath(ffprobe.path)
+                        .setFfmpegPath(ffmpegInstaller.path)
+                    
+                    await new Promise((resolve, reject) => {
+                        ffmpeg
+                            .input(videoStream)
+                            .noAudio()
+                            .fps(16)
+                            .size('250x?')
+                            .aspect('1:1')
+                            .keepDAR()
+                            .duration(6)
+                            .videoFilters([`crop=w='min(min(iw\,ih)\,512)':h='min(min(iw\,ih)\,512)'`])
+                            .format('webp')
+                            .output(tempName)
+                            .on('end', () => {
+                                console.log("Finished Generate from video")
+                                resolve(tempName)
+                            })
+                            .on('error', (e) => {
+                                console.log(e)
+                                reject(e)
+                            })
+                            .run()
+                    })
+
+                    const bufferwebp = fs.readFileSync(tempName)
+
+                    await this.send(id, <any>bufferwebp, MessageType.sticker)
+                    await fs.unlinkSync(tempName)
+                    await tempObject.removeCallback()
+                }
+            }
+        })
+        return this
+    }
+
+    public attachHeroes () {
+        console.log('attached: !heroes')
+        this.connection.on('chat-update', async (chat: WAChatUpdate) => {
+            if (chat.messages && chat.count) {
+                const message = chat.messages.last
+                const [ messageType ] = Object.keys(<any>message.message)
+                const id: string = <string>message.key.remoteJid
+                const text =  message.message?.conversation || {}
+
+                if (messageType == MessageType.text && text == '!heroes') {
+                    const rows = [
+                        {title: 'Iron Man', description: "Robert Downey Jr.", rowId:"rowid1"},
+                        {title: 'Captain America', description: "Chris Evans", rowId:"rowid2"},
+                        {title: 'Thor', description: "Chris Hemsworth", rowId:"rowid1"},
+                        {title: 'Spiderman', description: "Tom Hollan", rowId:"rowid1"},
+                        {title: 'Aquaman', description: "Jason Momoa", rowId:"rowid1"},
+                        {title: 'Quicksilver', description: "Evan Peters", rowId:"rowid1"},
+                        {title: 'Batman', description: "Ben Affleck", rowId:"rowid1"}
+                    ]
+                                        
+                    const button = {
+                        buttonText: 'Opciones',
+                        description: "Cual es tu super heroe favorito?",
+                        sections: [ {title: "Super Heroes", rows: rows} ],
+                        listType: 1
+                    }
+                    
+                    await this.send(id, <any>button, MessageType.listMessage)
+                }
+            }
+        })
+        return this
+    }
+
+    public attachPlan () {
+        console.log('attached: !plan')
+        this.connection.on('chat-update', async (chat: WAChatUpdate) => {
+            if (chat.messages && chat.count) {
+                const message = chat.messages.last
+                const [ messageType ] = Object.keys(<any>message.message)
+                const id: string = <string>message.key.remoteJid
+                const text =  message.message?.conversation || {}
+
+                if (messageType == MessageType.text && text == '!plan') {
+                   const buttons = [
+                        {buttonId: 'yes', buttonText: {displayText: 'Contrar'}, type: 1},
+                        {buttonId: 'no', buttonText: {displayText: 'Cancelar'}, type: 1}
+                    ]
+                    
+                    const buttonMessage = {
+                        contentText: "No cuentas con un plan activo, deseas:",
+                        buttons: buttons,
+                        headerType: 1
+                    }
+                    await this.send(id, <any>buttonMessage, MessageType.buttonsMessage)
                 }
             }
         })
