58e1e4e21fcb20a1772d920edf8b09e6958df474
Ulises Tobar <ulises.se.tobar@gmail.com>
2023-03-24 11:47:34 -0600

    chore(app): :fire: clean old version

--- src/modules/shared/domain/interfaces/messageCommandBus.interface.ts
+++
@@ -1,8 +0,0 @@
-import { MessageData, SendData } from "./types";
-
-type Message = (data: MessageData) => void
-
-export interface MessageCommandBus {
-    attach(pattern: string, callback: (data: MessageData) => void): void
-    send(body: SendData): void
-}
